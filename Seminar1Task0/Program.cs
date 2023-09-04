a3a9ab960d93fbb9444b736f157c62f490c3348b
Ivan <highteck86@bk.ru>
2023-09-04 21:48:45 +0300

    Добавил папки

---
+++ Seminar1Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №0
+// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите число: ");
+
+string? inputNum = Console.ReadLine(); //??"0";
+// Проверяем, чтобы данные были не пустыми
+if (inputNum != null)
+{
+    
+    int num = int.Parse(inputNum);
+
+    int resalt = num*num;
+
+    Console.WriteLine(resalt);
+}
+
+// Console.WriteLine("Квадрат числа:"+(int)Math.Pow(int.Parse(inputNum), 2)); решение в 1у строчку
---
+++ Seminar1Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №5
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// int a = int.Parse(Console.ReadLine());
+// if (a < 0) a = -a;
+// for(int i = -a; i <= a; i++){
+//     Console.Write(i);
+//     Console.Write(" ");
+// }
+// int N = int.Parse(Console.ReadLine()??"0");
+// int minusN = -N;
+// for(int i = -N; i <= N; i++)
+// {
+//     Console.Write(i.ToString()+ " ");
+// }
+
+// int NumN = int.Parse(Console.ReadLine()??"0");
+// int evNumN = (-1)*NumN;
+
+// while (evNumN < NumN)
+// {
+//    Console.Write (evNumN + ", ");
+//    evNumN++;
+// }
+//   Console.WriteLine (NumN);
