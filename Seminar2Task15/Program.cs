47de23071d12333cb9124f2dacdefbea68f13578
Ivan <highteck86@bk.ru>
2023-09-11 18:36:47 +0300

    Задача 15* добавлена

--- Seminar2Task15/Program.cs
+++ Seminar2Task15/Program.cs
@@ -1,24 +1,82 @@
 ﻿// Задача №15 Напишите программу, которая принимает на вход цифру, 
 // обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.WriteLine("Введите число");
-int day = int.Parse(Console.ReadLine()??"0");
+void Variant1()
 
-string [] dayOfWeek = new string[7];
+{
+    Console.WriteLine("Введите число");
+    int day = int.Parse(Console.ReadLine() ?? "0");
 
-dayOfWeek[0]="Понедельник";
-dayOfWeek[1]="Вторник";
-dayOfWeek[2]="Среда";
-dayOfWeek[3]="Четверг";
-dayOfWeek[4]="Пятница";
-dayOfWeek[5]="Суббота";
-dayOfWeek[6]="Воскресенье";
-if (day > 4 )
-{
-    Console.WriteLine(dayOfWeek[day-1]);
-    Console.Write("Выходной");
+    string[] dayOfWeek = new string[7];
+
+    dayOfWeek[0] = "Понедельник";
+    dayOfWeek[1] = "Вторник";
+    dayOfWeek[2] = "Среда";
+    dayOfWeek[3] = "Четверг";
+    dayOfWeek[4] = "Пятница";
+    dayOfWeek[5] = "Суббота";
+    dayOfWeek[6] = "Воскресенье";
+    if (day > 4)
+    {
+        Console.WriteLine(dayOfWeek[day - 1]);
+        Console.Write("Выходной");
+    }
+    else
+    {
+        Console.WriteLine(dayOfWeek[day - 1]);
+    }
 }
-else 
+void Variant2()
 {
-    Console.WriteLine(dayOfWeek[day-1]);
+Dictionary <int, string> dict = new Dictionary<int, string>();
+dict.Add(1, "Понедельник");
+dict.Add(2, "Вторник");
+dict.Add(3, "Среда");
+dict.Add(4, "Четверг");
+dict.Add(5, "Пятница");
+dict.Add(6, "Суббота");
+dict.Add(7, "Воскресение");
+// Другой способ ввести Dictionary
+// var days = new Dictionary <int, string>();
+
+// days [1] ="понедельник";
+// days [2] ="вторник";
+// days [3] ="среда";
+// days [4] ="четверг";
+// days [5] ="пятница";
+// days [6] ="суббота";
+// days [7] ="воскресение";
+
+// Другой способ ввести Dictionary
+// {
+//   {1, "Понедельник"},
+//   {2, "Вторник"},
+//   {3, "Среда"},
+//   {4, "Четверг"},
+//   {5, "Пятница"},
+//   {6, "Суббота"},
+//   {7, "Воскресение"},
+// };
+
+int i = Convert.ToInt32(Console.ReadLine());
+if (i > 5)
+{
+    Console.Write (dict[i] + " Выходной");
+    //Console.Write (dict[i]);
+    //Console.Write (" Выходной");
+        
 }
+else
+{
+Console.WriteLine (dict[i]);
+}
+// Выводит в столбец все дни недели
+// for (int i = 0; i < 8; i++)
+// {
+//    string value = dict.GetValueOrDefault(i);
+//    Console.WriteLine(value);
+// }
+}
+
+// Variant1();
+Variant2();
